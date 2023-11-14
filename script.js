7ae03cd636189835e46e05a36d886e8a0f6aa385
unknown <mertyasin1644@gmail.com>
2023-11-14 12:22:05 +0300

    ülkeler arrayini döndük

--- script.js
+++ script.js
@@ -1,1 +1,58 @@
 // ülkelerle çalışmak
+
+// console.log(countries_data)
+
+// for (let i = 0; i < countries_data.length; i++) {
+
+//     const p = document.createElement('p')
+//     p.textContent = `${countries_data[i].name} - ${countries_data[i].capital}`
+//     const img = document.createElement('img')
+//     img.src = countries_data[i].flag
+
+
+//     document.body.append(p)
+//     document.body.appendChild(img)
+// }
+
+
+
+// const p = document.createElement('p')
+// console.log(p)
+
+countries_data.forEach((ulke) => {
+    console.log(ulke)
+
+    const div = document.createElement('div')
+    div.classList.add('card')
+    div.style.width = "18rem"
+
+    const img = document.createElement('img')
+    img.src = ulke.flag
+    img.classList.add('card-img-top')
+
+    const div2 = document.createElement('div')
+    div2.classList.add('card-body')
+
+    const h5 = document.createElement('h5')
+    h5.classList.add('card-title')
+    h5.textContent = ulke.name
+
+    const p = document.createElement('p')
+    p.classList.add('card-text')
+    p.textContent = `${ulke.capital} - ${ulke.languages} - ${ulke.region}`
+
+    const a = document.createElement('a')
+    a.setAttribute('class', 'btn btn-warning')
+    a.textContent = ulke.population
+
+    div2.append(h5)
+    div2.append(p)
+    div2.append(a)
+
+    div.append(img)
+    div.append(div2)
+
+    document.body.append(div)
+
+
+})
